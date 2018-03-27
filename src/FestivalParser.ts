b57bc4b10b857396fd0fa1218e9ea2adcca1d626
elertan <denkievits@gmail.com>
2018-03-27 23:13:20 +0200

    Naam bugfix

--- src/FestivalParser.ts
+++ src/FestivalParser.ts
@@ -4,15 +4,24 @@ import Location from './models/Location';
 export default class FestivalParser {
     // tslint:disable-next-line: no-any
     public parse(data: any): Festival {
+        const venue = data._embedded.venues[0];
+        let address =
+            venue.postalCode 
+            + ', ' + 
+            venue.city.name;
+        if (venue.name !== undefined) {
+            address = venue.name + ', ' + address;
+        }
+
         return new Festival({
             name: data.name,
             url: data.url,
             location: new Location({
-                latitude: data._embedded.venues[0].location.latitude,
-                longitude: data._embedded.venues[0].location.longitude
+                latitude: venue.location.latitude,
+                longitude: venue.location.longitude
             }),
             imageUrl: data.images[0].url,
-            address: data._embedded.venues[0].name + ', ' + data._embedded.venues[0].postalCode + ', ' + data._embedded.venues[0].city.name,
+            address,
             startDate: new Date(data.sales.public.startDateTime),
             endDate: new Date(data.sales.public.endDateTime),
             ticketPrice: data.priceRanges[0].min,
